948094d1578ca7a429b9f88b31146215ff0e185e
Pratham Savjani <87868351+PrathamSavjani@users.noreply.github.com>
2021-11-22 02:11:57 +0530

    added 31 and 31

---
+++ 32.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int Partition(int *A, int start, int end)
+{
+
+    int pivot = A[end];
+
+    int partitionIndex = start; // set partition index as start initially
+
+    for (int i = start; i < end; i++)
+    {
+        if (A[i] <= pivot)
+        {
+            swap(A[i], A[partitionIndex]); // swap if element is lesser than pivot
+            partitionIndex++;
+        }
+    }
+    swap(A[partitionIndex], A[end]); // swap pivot with element at partition index
+    return partitionIndex;
+}
+
+void QuickSort(int *A, int start, int end)
+{
+
+    if (start < end)
+    {
+
+        int partitionIndex = Partition(A, start, end); // calling partition
+
+        QuickSort(A, start, partitionIndex - 1);
+        QuickSort(A, partitionIndex + 1, end);
+    }
+}
+
+int main()
+{
+    int A[] = {7, 6, 5, 4, 3, 2, 1, 0};
+    QuickSort(A, 0, 7);
+    for (int i = 0; i < 8; i++)
+        cout << A[i] << " ";
+}
